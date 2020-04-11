dcc83250996aff41a73e45929578aed6ac527260
Robert Giese <rwg1277@gmail.com>
2020-04-11 13:44:56 -0500

    First Commit

---
+++ Instructions/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "bc619c68a868632355d941103a6225ef"
+
+# Google API Key
+g_key = "AIzaSyBEujZ3D0qFWbmAHuBo6Q_KDNT7ZzPewSM"
---
+++ WeatherPy/config.py
@@ -0,0 +1,5 @@
+# Enter your API keys
+
+weather_api_key = "bc619c68a868632355d941103a6225ef"
+
+google_maps_key = "AIzaSyBEujZ3D0qFWbmAHuBo6Q_KDNT7ZzPewSM"
